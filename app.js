f37d0e7ae6a75a9416cb3954d23f91798c960d38
vairocana774 <vairocana774@gmail.com>
2020-01-29 00:10:50 +0900

    都道府県人工ランキングの実装完了

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ rl.on('line', (lineString) => {
       return pair2[1].change - pair1[1].change;
     });
     const rankinStrings = rankingArray.map(([key,value])=>{
-      return key + ':' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change ;
+      return `${key}: ${value.popu10} -> ${value.popu15} 変化率： ${value.change}`;
     });
     console.log(rankinStrings);
   });
